7fb309d56209fba003f23e7bd33912ce8c7dd9b7
Mariah <mariahschock@gmail.com>
2022-08-15 15:12:11 -0700

    creates and logs in new user, test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,38 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
+// const UserService = require('../lib/services/UserService');
+
+const userTest = {
+  email: 'usertest@example.com',
+  password: '123456',
+};
+
+// const registerAndLogin = async (userProps = {}) => {
+//   const agent = request.agent(app);
+//   const user = await UserService.create({ ...userTest, ...userProps });
+
+//   return [agent, user];
+// };
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+ 
+  it('POST - creates new user and signs user in', async () => {
+    const res = await request(app).post('/api/v1/users').send(userTest);
+    const { email } = userTest;
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      message: 'Signed in successfully',
+      user: { id: expect.any(String),
+        email,
+      }
+    });
   });
+
   afterAll(() => {
     pool.end();
   });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,26 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS restaurants CASCADE;
+DROP TABLE IF EXISTS reviews;
+
+CREATE TABLE users (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    email TEXT NOT NULL,
+    password_hash TEXT NOT NULL
+);
+
+CREATE TABLE reviews (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    review VARCHAR NOT NULL,
+    user_id BIGINT,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE TABLE restaurants (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR,
+    review_id BIGINT,
+    FOREIGN KEY (review_id) REFERENCES reviews (id)
+);
+

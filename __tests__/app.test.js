19c5ad134616f07e2f28aabd0f1cb0a6aa53058a
Mariah <mariahschock@gmail.com>
2022-08-16 12:35:22 -0700

    able to see list of users is logged in as admin

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,19 +2,24 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-// const UserService = require('../lib/services/UserService');
+const UserService = require('../lib/middleware/UserService');
 
 const userTest = {
   email: 'usertest@example.com',
   password: '123456',
 };
 
-// const registerAndLogin = async (userProps = {}) => {
-//   const agent = request.agent(app);
-//   const user = await UserService.create({ ...userTest, ...userProps });
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? userTest.password;
 
-//   return [agent, user];
-// };
+  const agent = request.agent(app);
+  const [user] = await UserService.create({ ...userTest, ...userProps });
+  const { email } = user;
+  console.log(user);
+  const response = await agent.post('/api/v1/users/sessions').send({ email, password });
+  console.log(response.body);
+  return [agent, user];
+};
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
@@ -37,6 +42,14 @@ describe('backend-express-template routes', () => {
     await request(app).post('/api/v1/users').send(userTest);
     const res = await request(app).post('/api/v1/users/sessions').send({ email: 'usertest@example.com', password: '123456' });
     expect(res.status).toBe(200);
+  });
+
+  it('GET - should return a list of users if signed in as admin', async () => {
+    const [agent, user] = await registerAndLogin({ email: 'admin@example.com' });
+    console.log(user);
+    const res = await agent.get('/api/v1/users');
+
+    expect(res.body).toEqual([{ ...user }]);
   });
 
   afterAll(() => {
